779b44b7d795bf0a22ea7ad11f78b6031cde6ad1
Mirik9724 <fu@fu.flo>
2025-04-26 19:58:19 +1000

    на сайт

--- StuDormPlusPlus/src/Strings.js
+++ StuDormPlusPlus/src/Strings.js
@@ -1,2 +1,2 @@
-let ip_server = "http://prod-team-12-ecl1h2gh.hack.prodcontest.ru:8001/"
-let ip_client = "http://prod-team-12-ecl1h2gh.hack.prodcontest.ru:443/"
+let ip_server = "http://localhost:8001/"
+let ip_client = "http://localhost:443/"
