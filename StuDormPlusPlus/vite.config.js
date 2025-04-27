bb98cafa001d481e8fc6388ca4e942b18cf19eaf
Mirik9724 <fu@fu.flo>
2025-04-27 11:06:50 +1000

    новый докер2 и 4.9 смена порта на 3000

--- StuDormPlusPlus/vite.config.js
+++ StuDormPlusPlus/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Разрешает доступ к серверу с любого IP-адреса
+    port: 3000,        // Устанавливает порт для сервера
+    strictPort: true,  // Если порт занят, Vite завершит запуск
+  }
 })
