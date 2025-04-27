e1dc178bd4aedcbbae3d868dd4085f5d3f648b8e
Mirik9724 <fu@fu.flo>
2025-04-27 13:42:07 +1000

    новый докер2 и 4.15

--- StuDormPlusPlus/vite.config.js
+++ StuDormPlusPlus/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ['prod-team-12-ecl1h2gh.hack.prodcontest.ru'],
-    host: '0.0.0.0',
+    host: true,
     port: 80,
     strictPort: true,
   }
